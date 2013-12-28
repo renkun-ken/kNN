f649d1d7c13f279deef308c4694dc5f18e8dcd07
Kun Ren <renkun@outlook.com>
2013-12-28 20:01:00 +0800

    test data and preliminary code
    the code does not work yet

--- src/classic-knn.R
+++ src/classic-knn.R
@@ -1,1 +1,23 @@
 # Classic k-nearest neighbor algorithms
+data <- read.csv("data/eurusd1.csv",header=F,stringsAsFactors=F)
+colnames(data) <- c("date","time","open","high","low","close","volume")
+plot(data$close,type="l")
+
+kpredict <- function(data,lens=seq(10,30),k=10) {
+  N <- length(data)
+  predict.df.list <- lapply(lens,function(len) {
+    series <- data[(N-len+1):N]
+    corr.df.list <- lapply(1:(N-len),function(i) {
+      data.frame(length=len,index=i,cor=cor(data[i:(i+len-1)],series),predict=data[i+len])
+    })
+    corr.df <- data.frame(do.call("rbind",corr.df.list))
+    corr <- corr.df[with(corr.df,order(abs(cor),decreasing=T)),][1:k,]
+    # baseline adjustment
+    predictor <- exp(corr$cor)*corr$predict/sum(exp(corr$cor))
+    predict.sd <- sd(exp(corr$cor)*corr$predict/sum(exp(corr$cor)))
+    data.frame(length=len,predict.sd=predict.sd)
+  })
+  predict.df<- do.call("rbind",predict.df.list)
+  result <- predict.df[with(predict.df,order(predict.sd)),]
+  result
+}
